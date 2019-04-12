55b1348bee329ad7c8d76eabe2ead44b108c5f9e
Sourav Mondal <souraviit776.bd@gmail.com>
2019-04-12 18:25:57 +0600

    Add files via upload

---
+++ RESTfulAPI_CRUD/Controllers/EmployeesController.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using EmployeeDataAccess;
+
+namespace RESTfulAPI_CRUD.Controllers
+{
+    public class EmployeesController : ApiController
+    {
+
+        public IEnumerable<Employee> Get()
+        {
+            using (EmployeeDBEntities entities = new EmployeeDBEntities())
+            {
+                return entities.Employees.ToList();
+            }
+        }
+        public HttpResponseMessage Get(int id)
+        {
+            using (EmployeeDBEntities entities = new EmployeeDBEntities())
+            {
+                var entity = entities.Employees.FirstOrDefault(x => x.ID == id);
+                if (entity != null)
+                {
+                    return Request.CreateResponse(HttpStatusCode.OK, entity);
+                }
+                else
+                {
+                    return Request.CreateResponse(HttpStatusCode.NotFound, "Employee with ID=" + id + " npot found!");
+                }
+            }
+
+        }
+        public HttpResponseMessage Post([FromBody] Employee emp)
+        {
+            try
+            {
+                using (EmployeeDBEntities entities = new EmployeeDBEntities())
+                {
+                    entities.Employees.Add(emp);
+                    entities.SaveChanges();
+                    var message = Request.CreateResponse(HttpStatusCode.Created, emp);
+                    message.Headers.Location = new Uri(Request.RequestUri + emp.ID.ToString());
+                    return message;
+                }
+            }
+            catch (Exception e)
+            {
+                return Request.CreateResponse(HttpStatusCode.BadRequest, e);
+            }
+        }
+        public HttpResponseMessage Delete([FromBody] int id)
+        {
+            try
+            {
+                using (EmployeeDBEntities entities = new EmployeeDBEntities())
+                {
+                    var entity = entities.Employees.FirstOrDefault(x => x.ID == id);
+                    if (entity == null)
+                    {
+                        return Request.CreateResponse(HttpStatusCode.NotFound, "Employee with ID=" + id + "not found to delete!");
+                    }
+                    else
+                    {
+                        entities.Employees.Remove(entity);
+                        entities.SaveChanges();
+                        return Request.CreateResponse(HttpStatusCode.OK, "");
+                    }
+
+                }
+            }
+            catch(Exception e)
+            {
+                return Request.CreateResponse(HttpStatusCode.BadRequest, e);
+            }
+        }
+        public HttpResponseMessage Put(int id,Employee emp)
+        {
+            try
+            {
+                using (EmployeeDBEntities entities = new EmployeeDBEntities())
+                {
+                    var entity = entities.Employees.FirstOrDefault(x => x.ID == id);
+                    if (entity == null)
+                    {
+                        return Request.CreateResponse(HttpStatusCode.NotFound, "Employee with ID=" + id + "not found to update!");
+                    }
+                    else
+                    {
+                        entity.FirstName = emp.FirstName;
+                        entity.LastName = emp.LastName;
+                        entity.Gender = emp.Gender;
+                        entity.Salary = emp.Salary;
+                        entities.SaveChanges();
+                        return Request.CreateResponse(HttpStatusCode.OK, entity);
+                    }
+                }
+            }
+            catch(Exception e)
+            {
+                return Request.CreateResponse(HttpStatusCode.BadRequest, e);
+            }
+        }
+    }
+}
+
---
+++ RESTfulAPI_CRUD/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="RESTfulAPI_CRUD.WebApiApplication" Language="C#" %>
